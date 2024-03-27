ea63f7be21d389e8a8ce90932b7cf7cf6a5b2989
Maximilian Gobbel <maxigobbel@googlemail.com>
2024-03-27 14:21:56 +0100

    testcommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
+/**
+ *
+ */
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>Meals</h1>
-        <p>test</p>
       </div>
     </main>
   );
