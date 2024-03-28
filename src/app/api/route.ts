a5c86a06b80ce74be94b77b5d8c8976f0c2307a8
Maximilian Gobbel <maxigobbel@googlemail.com>
2024-03-28 12:53:09 +0100

    Refactor GET function to handle error cases

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
  * @returns A Promise that resolves to a NextResponse object containing the fetched data if the request is successful, or an object with an error message if the request fails.
  * @throws Throws an error if the fetch request fails.
  */
-export async function GET() {
+export async function GET(): Promise<NextResponse> {
   try {
     const response = await fetch(
       "https://api.thecatapi.com/v1/images/search?limit=10",
@@ -18,7 +18,13 @@ export async function GET() {
     const data = await response.json();
 
     return NextResponse.json(data);
-  } catch (error) {
-    return { error: error.message };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "An unknown error occurred" },
+      { status: 500 },
+    );
   }
 }
