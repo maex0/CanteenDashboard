a0491d35b42e0e601462e6223466e8817249d95d
Maximilian Gobbel <maxigobbel@googlemail.com>
2024-06-10 11:56:13 +0200

    fixed tests, added layout test

---
+++ next/tests/unit/layout.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import RootLayout from "../../src/app/layout";
+
+describe("RootLayout", () => {
+  it("renders its children", () => {
+    const { getByText } = render(
+      <RootLayout>
+        <div>Child Component</div>
+      </RootLayout>,
+    );
+
+    expect(getByText("Child Component")).toBeInTheDocument();
+  });
+});
